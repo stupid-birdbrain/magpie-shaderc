b77a2b62530ff7b3169c787c8822c726d33a760e
mayli4 <sydsleeper@gmail.com>
2025-12-02 02:33:04 -0500

    removal: remove ReflectedMember.cs

--- src/ShaderCompilation/Models/ReflectedMember.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShaderCompilation.Models;
-
-public readonly struct ReflectedMember(string name, ShaderDataType type, uint offset, uint size, uint arrayElements) {
-    public readonly string Name = name;
-    public readonly ShaderDataType DataType = type;
-    public readonly uint Offset = offset;
-    public readonly uint Size = size;
-    public readonly uint ArrayElements = arrayElements; // 0 if not an array
-}
